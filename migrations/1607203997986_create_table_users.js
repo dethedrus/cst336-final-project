641e85e7325e09f03689fa1c8542cd19a13a5ca8
Jose Herrera <josevictorherrera@gmail.com>
2020-12-05 14:47:11 -0800

    Add DOWN for users seed

--- migrations/1607203997986_create_table_users.js
+++ migrations/1607203997986_create_table_users.js
@@ -1,6 +1,6 @@
 module.exports = {
+    // password: 'secret'
     "up": `INSERT INTO TABLE user (username, password)
-    VALUES ('jose', '$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6')
-    `, // password: 'secret'
-    "down": ""
+        VALUES ('jose', '$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6');`,
+    "down": "DELETE FROM user WHERE username='jose';"
 }
