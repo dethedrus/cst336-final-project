978b70e58b1a76558fac99effb8c3c89e8a5413e
Jose Herrera <josevictorherrera@gmail.com>
2020-12-04 19:50:38 -0800

    Change default to localhost

--- dbPool.js
+++ dbPool.js
@@ -2,7 +2,7 @@ const mysql = require("mysql")
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.MYSQL_HOST || "db",
+    host: process.env.MYSQL_HOST || "localhost",
     user: process.env.MYSQL_USER || "root",
     password: process.env.MYSQL_PASSWORD || "root",
     database: process.env.MYSQL_DATABASE || "cst336-final",
