30f9b15b7a2c0f8ca1b5c79f9db0b16709f8aa06
Jose Herrera <josevictorherrera@gmail.com>
2020-12-04 19:22:36 -0800

    Add routes and views

--- index.js
+++ index.js
@@ -19,8 +19,63 @@ app.use(formData.parse());
 
 // routes
 app.get("/", async function (req, res) {
+    // homepage
     var viewData = {}
     res.render("index", viewData)
+})
+app.get("/ebook", async function (req, res) {
+    // ebook index page; browse books
+    var viewData = {}
+    res.render("ebook-index", viewData)
+})
+app.get("/ebook/:ebook_id", async function (req, res) {
+    // ebook page; show detail for single ebook
+    var viewData = {}
+    res.render("ebook-show", viewData)
+})
+app.get("/api/ebook", async function (req, res) {
+    // return a list of ebooks available
+    res.send()
+})
+app.get("/api/ebook/:ebook_id", async function (req, res) {
+    // return single ebook detail
+    res.send()
+})
+app.get("/api/ebook/:ebook_id/checkout", async function (req, res) {
+    // user checks out a book
+    res.send()
+})
+app.get("/login", async function (req, res) {
+    // login page
+    var viewData = {}
+    res.render("login", viewData)
+})
+app.get("/api/login", async function (req, res) {
+    // login api call
+    res.send()
+})
+app.get("/api/logout", async function (req, res) {
+    // logout api call
+    res.send()
+})
+app.get("/user", async function (req, res) {
+    // user management page; 
+    var viewData = {}
+    res.render("user-index", viewData)
+})
+app.get("/user/ebook", async function (req, res) {
+    // user borrowed ebooks index; show list of borrowed books 
+    var viewData = {}
+    res.render("user-ebook-index", viewData)
+})
+app.get("/user/ebook/:borrow_id", async function (req, res) {
+    // user borrowed ebook; display ebook to user in browser
+    var viewData = {}
+    res.render("user-ebook-show", viewData)
+})
+app.get("/api/user/ebook/:borrow_id/return", async function (req, res) {
+    // user borrowed ebook; display ebook to user in browser
+    res.send()
 })
 
 // starting server
