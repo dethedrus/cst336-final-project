9edba04c902775536efa454784729f6fc18622fd
Jose Herrera <josevictorherrera@gmail.com>
2020-12-05 14:12:37 -0800

    Add ebook fields

--- migrations/1607139106297_create_ebook_table.js
+++ migrations/1607139106297_create_ebook_table.js
@@ -3,6 +3,9 @@ module.exports = {
         CREATE TABLE ebook(
             id INT NOT NULL AUTO_INCREMENT,
             title VARCHAR(255) NOT NULL,
+            author VARCHAR(255) NOT NULL,
+            cover_filename VARCHAR(255) NOT NULL,
+            filename VARCHAR(255) NOT NULL,
             gutenberg_id INT,
             created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             PRIMARY KEY (id),
