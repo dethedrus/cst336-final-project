026c0441c72c14865cbe94cc2069f77170fa2ca6
Jose Herrera <josevictorherrera@gmail.com>
2020-12-09 09:02:09 -0800

    Update schema by default

--- migration.js
+++ migration.js
@@ -3,5 +3,5 @@ const migration = require("mysql-migrations")
 const connection = require("./dbPool.js")
 
 migration.init(connection, __dirname + "/migrations", function () {
-  console.log("Finished running migrations");
-})
+  console.log("Finished running migrations")
+}, ["--update-schema"])
