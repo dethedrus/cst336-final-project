799801ec6ad64e1f082f88f97ead5cec0a33b763
Jose Herrera <josevictorherrera@gmail.com>
2020-12-09 20:57:41 -0800

    Fix syntax typo

--- migrations/1607575917559_create_table_borrows.js
+++ migrations/1607575917559_create_table_borrows.js
@@ -2,7 +2,7 @@ module.exports = {
     "up": `
         INSERT INTO borrow (user_id, ebook_id)
         VALUES 
-            (1, 1, 1,),
+            (1, 1, 1),
             (2, 2, 2),
             (3, 3, 3),
             (4, 4, 4)
