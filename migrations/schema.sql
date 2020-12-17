64dba265e078284261d57ce3bd9d4d520ad061a0
Jose Herrera <josevictorherrera@gmail.com>
2020-12-17 08:11:10 -0800

    Fix seeds

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -49,7 +49,7 @@ CREATE TABLE `ebook` (
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `gutenberg_id` (`gutenberg_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,4 +93,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-10  5:00:40
+-- Dump completed on 2020-12-17 16:08:24
