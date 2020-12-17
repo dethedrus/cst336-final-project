efc11077ea58a57ad57b1b3771b7d49ff92bc28c
Jose Herrera <josevictorherrera@gmail.com>
2020-12-17 08:14:49 -0800

    Add admin account

--- migrations/1607203997986_create_table_users.js
+++ migrations/1607203997986_create_table_users.js
@@ -3,10 +3,11 @@ module.exports = {
     "up": `
         INSERT INTO user (id, username, password)
         VALUES 
-            (1, 'david', '$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6'),
-            (2, 'jose', '$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6'),
-            (3, 'jomar', '$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6'),
-            (4, 'yoela', '$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6')
+            (1, 'david', '$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6')
+            ,(2, 'jose', '$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6')
+            ,(3, 'jomar', '$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6')
+            ,(4, 'yoela', '$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6')
+            ,(5, 'admin', '$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6')
         ;
     `,
     "down": "DELETE FROM user WHERE id BETWEEN 1 AND 4;"
--- migrations/seed.sql
+++ migrations/seed.sql
@@ -3,11 +3,12 @@ TRUNCATE ebook;
 TRUNCATE user;
 
 INSERT INTO user (id, username, password)
-VALUES 
-    (1, 'david', '$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6'),
-    (2, 'jose', '$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6'),
-    (3, 'jomar', '$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6'),
-    (4, 'yoela', '$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6')
+    VALUES 
+        (1, 'david', '$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6')
+        ,(2, 'jose', '$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6')
+        ,(3, 'jomar', '$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6')
+        ,(4, 'yoela', '$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6')
+        ,(5, 'admin', '$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6')
 ;
 
 
